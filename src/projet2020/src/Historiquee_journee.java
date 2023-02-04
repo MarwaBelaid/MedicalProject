a5c21839278bf9dba01993d47a5033cf33addad6
sedki belhadj <sedki.belhadj@esprit.tn>
2023-02-04 09:41:07 +0100

    implementation de la classe historique_journee

--- src/projet2020/src/Historiquee_journee.java
+++ src/projet2020/src/Historiquee_journee.java
@@ -24,7 +24,7 @@
 
 /*
  * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
+ * To change this template file, choose Tools | Templatesjjj
  * and open the template in the editor.
  */
 package projet2020;
@@ -36,7 +36,7 @@ import java.util.Date ;
 import javax.swing.table.DefaultTableModel;
 /**
  *
- * @author Zohra
+ * @author sedki
  */
 public class historique_journee extends javax.swing.JFrame {
 
